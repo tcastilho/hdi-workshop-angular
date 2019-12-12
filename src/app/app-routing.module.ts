203efc421c5ef644c893aa6f9f56070503d543e7
Thiago Castilho <thiagocastilho@me.com>
2019-12-12 14:36:19 -0300

    auth guard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StarwarsComponent } from './components/starwars/starwars.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
     path: 'starwars',
-    component: StarwarsComponent
-  }
+    component: StarwarsComponent,
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/starwars',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,19 +8,30 @@ import { AppComponent } from './app.component';
 import { AppBootstrapModule } from './modules/app-bootstrap/app-bootstrap.module';
 import { StarwarsComponent } from './components/starwars/starwars.component';
 import { StarwarsService } from './services/starwars.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StarwarsComponent
+    StarwarsComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     AppBootstrapModule
   ],
-  providers: [StarwarsService],
+  providers: [
+    StarwarsService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
